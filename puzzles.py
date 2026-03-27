dae9bd7ebc8b9480e28b79f38c8d6c8188972e53
CHIDO-X <146003512+CHIDO-X@users.noreply.github.com>
2026-03-28 03:20:03 +0530

    Added layout for Medium Puzzle and Hard Puzzle.

--- puzzles.py
+++ puzzles.py
@@ -33,3 +33,50 @@ def _puzzler_easy() -> GameState:
     Car('X', 2, 0, 2, 'H'), #Red Car must reach column 4
     Car('A', 1, 3, 2, 'V'), #Car A blocks column 3 in row 2
   ])
+
+# MEDIUM PUZZLE
+
+def _puzzle_medium() -> GameState:
+  """
+  Board Layout:
+
+  A A . . . .
+  . . . B . .
+  X X . B . C -> EXIT
+  . . . . . C
+  . . D D . .
+  . . . . . .
+
+  Involves moving Mutiple Cars.
+  Solution: Car B moves vertically up, and car C moves ertically up/ down. Path for Red Car to exit is cleared in this manner. """
+  return GameState([
+        Car('X', 2, 0, 2, 'H'),   # Red car
+        Car('A', 0, 0, 2, 'H'),   # Top-left horizontal
+        Car('B', 1, 3, 2, 'V'),   # Blocks col 3 in row 2
+        Car('C', 2, 5, 2, 'V'),   # Blocks col 5 (the exit column!)
+        Car('D', 4, 2, 2, 'H'),   # Bottom horizontal
+    ])
+
+# HARD PUZZLE
+
+def _puzzle_hard() -> GameState:
+  """
+  Board layout:
+
+  . . A . B B
+  . . A . . .
+  X X A . . . -> EXIT
+  . . . C C .
+  . D D . . .
+  . . . E E .
+
+  Requires 8+ moves to free Red Car.
+  Solution: D must move first to let A slide down, which unblocks X. Requires sliding Car D to free up space for car A. """
+  return GameState([
+        Car('X', 2, 0, 2, 'H'),   # Red Car
+        Car('A', 0, 2, 3, 'V'),   # Tall blocker Car in column 2
+        Car('B', 0, 4, 2, 'H'),   # Top-right horizontal Car
+        Car('C', 3, 3, 2, 'H'),   # Mid-right horizontal Car
+        Car('D', 4, 1, 2, 'H'),   # Blocks A from sliding down
+        Car('E', 5, 3, 2, 'H'),   # Bottom horizontal Car
+    ])
