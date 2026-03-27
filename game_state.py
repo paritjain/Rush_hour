5e6e8d2dcffd7dd4e204051c940686ca7fe4aa47
harupats <sirfharshit@gmail.com>
2026-03-27 13:16:26 +0530

    Create Car class with attributes

--- game_state.py
+++ game_state.py
@@ -1,1 +1,16 @@
+class Car:
+    """A single vehicle on the Rush Hour grid."""
 
+    def __init__(self, car_id: str, row: int, col: int, length: int, orientation: str):
+     """
+        car_id      : single character label ('X', 'A', 'B', …)
+        row, col    : top-left corner of the car (0-indexed)
+        length      : number of cells occupied (2 or 3)
+        orientation : 'H' = horizontal, 'V' = vertical
+      """
+        self.car_id      = car_id
+        self.row         = row
+        self.col         = col
+        self.length      = length
+        self.orientation = orientation 
+#Helpers
