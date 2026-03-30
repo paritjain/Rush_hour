f6784bd467701af9ee8b95a51bb50bcc0e7c651f
harupats <sirfharshit@gmail.com>
2026-03-30 21:11:20 +0530

    improved the indentation in car class

--- game_state.py
+++ game_state.py
@@ -1,19 +1,17 @@
 class Car:
     """A single vehicle on the Rush Hour grid."""
-
-    def __init__(self, car_id: str, row: int, col: int, length: int, orientation: str):
-     """
+       def __init__(self, car_id: str, row: int, col: int, length: int, orientation: str):
+            """
         car_id      : single character label ('X', 'A', 'B', …)
         row, col    : top-left corner of the car (0-indexed)
         length      : number of cells occupied (2 or 3)
         orientation : 'H' = horizontal, 'V' = vertical
       """
-        self.car_id      = car_id
-        self.row         = row
-        self.col         = col
-        self.length      = length
-        self.orientation = orientation 
-        
+        self.car_id = car_id
+        self.row = row
+        self.col = col
+        self.length = length
+        self.orientation = orientation
 #Helpers
    def cells(self) -> list:
         """Return every (row, col) tuple this car occupies."""
